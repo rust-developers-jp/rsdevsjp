38ffd49a6e408bafda69517a4ddd48382b344ad6
Rion <info@ri0n.dev>
2026-06-12 01:57:12 +0900

    chore: add assetPrefix and basePath configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const urlPrefix = process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : ''
+
 const nextConfig: NextConfig = {
   output: "export",
+  assetPrefix: urlPrefix,
+  basePath: urlPrefix,
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
