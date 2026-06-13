1fb2615a7619432dca07d3743136c9bcd49dfe87
Rion <info@ri0n.dev>
2026-06-13 19:28:13 +0900

    fix: resolve redirect issue

--- next.config.ts
+++ next.config.ts
@@ -10,15 +10,6 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
-  async redirects() {
-    return [
-      {
-        source: "/join",
-        destination: "https://discord.gg/yrezWw4r9r",
-        permanent: false,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
