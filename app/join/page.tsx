fe087755f2926fbadc6ea5e9e515e869cb95d358
Kai Tomotake <kaitomotake@gmail.com>
2026-08-13 15:28:25 +0900

    Fix discord join link

--- app/join/page.tsx
+++ app/join/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function JoinPage() {
-	redirect("https://discord.gg/yrezWw4r9r");
+	redirect("https://discord.com/invite/KDCDNtxzB9");
 }
